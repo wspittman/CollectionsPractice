1c5621b39b3133d75383491abc541f17413215e9
William Pittman <wspittman@gmail.com>
2021-03-24 13:32:57 -0700

    Add Sorts skeleton

---
+++ CollectionsPractice/Sorts.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CollectionsPractice
+{
+    public static class Sorts
+    {
+        public static void MergeSort(int[] array)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void QuickSort(int[] array)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void RadixSort(int[] array)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
