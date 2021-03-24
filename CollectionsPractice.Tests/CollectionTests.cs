28dc2bfd540598fa590deebb814ff53a574ba5e3
William Pittman <wspittman@gmail.com>
2021-03-24 12:55:22 -0700

    Add different initial states

--- CollectionsPractice.Tests/CollectionTests.cs
+++ CollectionsPractice.Tests/CollectionTests.cs
@@ -6,11 +6,12 @@ namespace CollectionsPractice.Tests
     public class CollectionTests
     {
         public enum TestableType { ArrayList, SortedArrayList, LinkedList, ArrayStack, LinkedQueue, BinarySearchTree }
+        public enum InitType { Empty, Single, Asc, Desc, Mixed }
 
         [Test]
-        public void TestAdd([Values] TestableType testableType)
+        public void TestAdd([Values] TestableType testableType, [Values] InitType initType)
         {
-            var (expected, collection) = Startup(testableType);
+            var (expected, collection) = Startup(testableType, initType);
 
             expected.Add(1);
             collection.Add(1);
@@ -32,9 +33,17 @@ namespace CollectionsPractice.Tests
             Check(expected, collection);
         }
 
-        private (ITestCollection, ITestCollection) Startup(TestableType testableType)
+        private (ITestCollection, ITestCollection) Startup(TestableType testableType, InitType initType)
         {
-            var initial = new int[] { 1 };
+            var initial = initType switch
+            {
+                InitType.Empty => new int[] { },
+                InitType.Single => new int[] { 1 },
+                InitType.Asc => new int[] { -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5 },
+                InitType.Desc => new int[] { 5, 4, 3, 2, 1, 0, -1, -2, -3, -4, -5 },
+                InitType.Mixed => new int[] { 0, 2, -2, 5, -4, 1, 3, -1, -3, 4, -5 },
+                _ => throw new ArgumentException("Unknown InitType"),
+            };
 
             ITestCollection collection = testableType switch
             {
